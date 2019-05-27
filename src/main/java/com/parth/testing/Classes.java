caf119745e3d455f7364a114eaa07309727487aa
Parth Upadhyay <parth.upadhyay@gmail.com>
2019-05-27 18:32:58 +0900

    add better examples

--- src/main/java/com/parth/testing/Classes.java
+++ src/main/java/com/parth/testing/Classes.java
@@ -7,8 +7,17 @@ class TestClassLombok {
     // This variable does not get translated correctly.
     int xPosition;
 
+    // This variable translates correctly starting from the 2nd word.
+    int aLongerVariableName;
+
     // This variable does get translated correctly.
     int abPosition;
+}
+
+class TestClassPublic {
+    // Public variables all get translated correctly
+    public int xPosition;
+    public int abPosition;
 }
 
 class TestClassGetterDifference {
